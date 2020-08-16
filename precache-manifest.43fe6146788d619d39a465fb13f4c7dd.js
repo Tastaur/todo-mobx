f3b531cc108430de0b5ff58b46e562e6a889c29f
Mark <Trrrrex93@gmail.com>
2020-08-16 22:29:44 +0400

    Updates

---
+++ precache-manifest.43fe6146788d619d39a465fb13f4c7dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e28de50354cff77e3c4f34c9bfec1be",
+    "url": "/todo-mobx/index.html"
+  },
+  {
+    "revision": "9e2fabc26e30d61ed166",
+    "url": "/todo-mobx/static/css/main.f18736ab.chunk.css"
+  },
+  {
+    "revision": "d7f5b6221d25faa9ff3e",
+    "url": "/todo-mobx/static/js/2.a8527b4d.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/todo-mobx/static/js/2.a8527b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2fabc26e30d61ed166",
+    "url": "/todo-mobx/static/js/main.a9a19b46.chunk.js"
+  },
+  {
+    "revision": "5db96470ff4267a81bc7",
+    "url": "/todo-mobx/static/js/runtime-main.c86cbf6b.js"
+  }
+]);
